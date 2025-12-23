e8c1798c75303efc3c0cc6593ddf537440b75926
Willy-557 <williamfsetiadi@gmail.com>
2025-12-23 11:39:31 +0700

    Make the input for booking seat

--- booking.java
+++ booking.java
@@ -22,44 +22,53 @@ public class booking {
                 switch (opsi) {
                 case 1:
                     System.out.println("\nKursi yang sudah di-booking: ");
+                    System.out.println("'O' = Sudah diisi");
+                    System.out.println("'X' = Belum diisi\n");
                     for (int i = 0; i < kursi.length; i++) {
                         if (kursi[i] == null) {
                             int angka = i+1;
-                            System.out.println("Kursi ke-" + angka + ". : " + "❌");
+                            System.out.println("Kursi ke-" + angka + ". : " + "[X]");
                         }
                         else {
                             int angka = i+1;
-                            System.out.println("Kursi ke-" + angka + ". : " + "✅️");
+                            System.out.println("Kursi ke-" + angka + ". : " + "[O]");
                         }
                     }
                     break;
 
                 case 2:
                     System.out.println("\nKursi yang tersedia di bioskop: ");
+                    System.out.println("'O' = Sudah diisi");
+                    System.out.println("'X' = Belum diisi\n");
                     for (int i = 0; i < kursi.length; i++) {
                         if (kursi[i] == null) {
                             int angka = i+1;
-                            System.out.println("Kursi ke-" + angka + ". : " + "❌");
+                            System.out.println("Kursi ke-" + angka + ". : " + "[X]");
                         }
                         else {
                             int angka = i+1;
-                            System.out.println("Kursi ke-" + angka + ". : " + "✅️");
+                            System.out.println("Kursi ke-" + angka + ". : " + "[O]");
                         }
                     }
 
                     System.out.print("Kursi mana yang anda ingin booking? : ");
                     int pilihanKursi = scanner.nextInt();
-                    int pilihanKursiFixed = pilihanKursi - 1;
-                    if (kursi[pilihanKursiFixed] == null) {
-                        System.out.println("Anda sudah sukses membooking kursi no " + pilihanKursi + ".");
-                        kursi[pilihanKursiFixed] = "✅️";
-                        break;
+                    if (pilihanKursi > kursi.length || pilihanKursi < 1) {
+                        System.out.println("\nInput tidak valid!");
                     }
                     else {
-                        System.out.println("Kursi yang anda pilih telah dibooking oleh orang lain!");
-                        continue;
+                        int pilihanKursiFixed = pilihanKursi - 1;
+                        if (kursi[pilihanKursiFixed] == null) {
+                            System.out.println("\nAnda sudah sukses membooking kursi no " + pilihanKursi + ".");
+                            kursi[pilihanKursiFixed] = "O";
+                            break;
+                        }
+                        else {
+                            System.out.println("\nKursi yang anda pilih telah dibooking oleh orang lain!");
+                            continue;
+                        }
                     }
-
+                
                 default:
                     break;
             }
