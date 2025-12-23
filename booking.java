2ec99716fd8da4ffb151f2a80e9d0c213723fc38
Willy-557 <williamfsetiadi@gmail.com>
2025-12-23 11:32:57 +0700

    Input the emoji for check is the seat available or not

--- booking.java
+++ booking.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class booking {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner (System.in);
         String[] kursi = new String[10];
         
         while (true){
-            System.out.println("=== Bioskop ===");
+            System.out.println("\n=== Bioskop ===");
             System.out.println("1. Tampilkan denah kursi");
             System.out.println("2. Booking kursi");
             System.out.println("3. Keluar");
@@ -20,28 +21,45 @@ public class booking {
             else {
                 switch (opsi) {
                 case 1:
+                    System.out.println("\nKursi yang sudah di-booking: ");
                     for (int i = 0; i < kursi.length; i++) {
                         if (kursi[i] == null) {
-                            System.out.println("[   ]");
+                            int angka = i+1;
+                            System.out.println("Kursi ke-" + angka + ". : " + "❌");
                         }
                         else {
-                            System.out.println("[ X ]");
+                            int angka = i+1;
+                            System.out.println("Kursi ke-" + angka + ". : " + "✅️");
                         }
                     }
                     break;
 
                 case 2:
+                    System.out.println("\nKursi yang tersedia di bioskop: ");
                     for (int i = 0; i < kursi.length; i++) {
                         if (kursi[i] == null) {
                             int angka = i+1;
-                            System.out.println("Kursi ke-" + angka + ". : " + "[   ]");
+                            System.out.println("Kursi ke-" + angka + ". : " + "❌");
                         }
                         else {
                             int angka = i+1;
-                            System.out.println("Kursi ke-" + angka + ". " + "[ X ]");
+                            System.out.println("Kursi ke-" + angka + ". : " + "✅️");
                         }
                     }
-                
+
+                    System.out.print("Kursi mana yang anda ingin booking? : ");
+                    int pilihanKursi = scanner.nextInt();
+                    int pilihanKursiFixed = pilihanKursi - 1;
+                    if (kursi[pilihanKursiFixed] == null) {
+                        System.out.println("Anda sudah sukses membooking kursi no " + pilihanKursi + ".");
+                        kursi[pilihanKursiFixed] = "✅️";
+                        break;
+                    }
+                    else {
+                        System.out.println("Kursi yang anda pilih telah dibooking oleh orang lain!");
+                        continue;
+                    }
+
                 default:
                     break;
             }
