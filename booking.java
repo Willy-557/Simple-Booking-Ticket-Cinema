0a2e13fbb4eb213ee5be862d90f8d3e8ea957760
Willy-557 <williamfsetiadi@gmail.com>
2025-12-23 11:05:58 +0700

    Make the cinema menu

---
+++ booking.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class booking {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        String[] kursi = new String[10];
+        
+        while (true){
+            System.out.println("=== Bioskop ===");
+            System.out.println("1. Tampilkan denah kursi");
+            System.out.println("2. Booking kursi");
+            System.out.println("3. Keluar");
+            System.out.print(">> ");
+            int opsi = scanner.nextInt();
+
+            if (opsi == 3){
+                System.out.println("Terimakasih!");
+                break;
+            }
+            else {
+                
+
+
+                default:
+                    break;
+            }
+            }
+
+            
+        }
+    }
+}
